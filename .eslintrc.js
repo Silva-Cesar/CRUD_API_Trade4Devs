6ad4b621d98413d8afced282b5c4a931ff42f159
César Silva <cesaraugusto90@gmail.com>
2022-05-23 17:00:17 -0300

    style: configurar Prettier e ESlint

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,9 +5,9 @@ module.exports = {
    },
    root: true,
    extends: [
-     'plugin:@typescript-eslint/recommended',
-     'prettier/@typescript-eslint',
-     'airbnb-base',
+    'plugin:@typescript-eslint/eslint-recommended', // Usa as regras recomendadas do @typescript-eslint/eslint-plugin
+    'prettier/@typescript-eslint', // Usa eslint-config-prettier para desabilitar regras ESLint de @typescript-eslint/eslint-plugin que entraria em conflito com Prettier
+    'plugin:prettier/recommended', // Ativa o eslint-plugin-prettier e exibe erros Prettier como erros ESLint. Certifique-se de que esta seja sempre a última configuração no array
    ],
    parser: '@typescript-eslint/parser',
    parserOptions: {
