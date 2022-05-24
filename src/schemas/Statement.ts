4610f282bc68e29585f82188d812180b54fdfd7a
Andre <andre@stratobits.com.br>
2022-05-24 03:35:02 -0300

    API extrato

---
+++ src/schemas/Statement.ts
@@ -0,0 +1,43 @@
+import { model, Schema, Document, ObjectId } from 'mongoose';
+
+export interface StatementInterface extends Document {
+  cpf: Number;
+  month: Number;
+  year: Number;
+  operation: [ ObjectId ];  
+  createdAt: Date;
+  updatedAt: Date;
+  deletedAt: Date;
+}
+
+const StatementSchema = new Schema({
+  cpf: {
+    type: Number,
+    //min: 11,
+    //max: 12,
+    required: [true, 'CPF is required']
+  },
+  month: {
+    type: Number,
+    //min: 1,
+    //max: 12,
+    required: [true, 'Month is required']
+  },
+  year: {
+    type: Number,
+    required: [true, 'Year is required']
+  },
+  operations : [{
+     type : Schema.Types.ObjectId, ref: 'Operation', 
+   required: [true, 'At least one operation is required'] 
+  }],
+  deletedAt: {
+    type: Date,
+    default: null
+  }
+},
+{
+  timestamps: true
+});
+
+export default model<StatementInterface>('Statement', StatementSchema);
--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,15 @@ import UserController from './controllers/UserController';
 import OperationController from './controllers/OperationController';
 import BalanceController from './controllers/BalanceController';
 import RegisterController from './controllers/RegisterController';
+import StatementController from './controllers/StatementController';
 
-const app = new App([ new ProductController(), new  UserController(), new OperationController(), new BalanceController(), new RegisterController() ]);
+const app = new App([ 
+    new ProductController(), 
+    new  UserController(), 
+    new OperationController(), 
+    new BalanceController(), 
+    new RegisterController(), 
+    new StatementController()
+]);
 
 app.listen(3000);
