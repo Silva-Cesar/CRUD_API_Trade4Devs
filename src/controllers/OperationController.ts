6379c97dca0c333bac456853840432c96bf7a590
Andre <andre@stratobits.com.br>
2022-05-26 06:53:48 -0300

    adicao do axios, operationcontroller cria operacao e envia pro extrato

--- src/controllers/OperationController.ts
+++ src/controllers/OperationController.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from 'express';
 import Operation from '../schemas/Operation';
 import Controller from './Controller';
-//import Product from '../schemas/Product';
+import { Types } from 'mongoose';
 
 
 class OperationController extends Controller {
@@ -23,27 +23,78 @@ class OperationController extends Controller {
   }
 
   private async create(req: Request, res: Response, next: NextFunction): Promise<Response> {
-    const bodyReq = req.body;
+    //const bodyReq = req.body;
 
-    if (bodyReq.cpf !== 22345678900) { // Validando CPF
+    const { id , sender , receiver } = await Operation.create(req.body);
+
+    if (!Types.ObjectId.isValid(id)) {
+      return res.status(400).send('Id Inválido');
+    } else {
+
+      // chamar api extrato
+
+      const axios = require('axios').default;
+      const date = new Date();
+
+      axios.post('http://127.0.0.1:3000/statement', {
+        cpf : sender,
+        year : date.getUTCFullYear(),
+        month : date.getUTCMonth(),
+        //operations : `[{${id}}]`
+        operations : [{_id : id}]
+      })
+      //.then(function (response) {
+      //  console.log(response);
+        .then(function (response: any) {
+          console.log(response);        
+      })
+      //.catch(function (error) {
+      //  console.log(error);
+        .catch(function (error: any) {
+          console.log(error);        
+      });
+
+      axios.post('http://127.0.0.1:3000/statement', {
+        cpf : receiver,
+        year : date.getUTCFullYear(),
+        month : date.getUTCMonth(),
+        //operations : `[{${id}}]`
+        operations : [{_id : id}]
+      })
+      //.then(function (response) {
+      //  console.log(response);
+        .then(function () {
+          console.log();        
+      })
+      //.catch(function (error) {
+      //  console.log(error);
+        .catch(function () {
+          console.log();        
+      });      
+
+      return res.status(200).send('ok');
+    }
+  }
+    
+    //if (bodyReq.cpf !== 22345678900) { // Validando CPF
       // Verificar CPF no registro.
-      return res.status(404).send('CPF não encontrado');
-    };
+    //  return res.status(404).send('CPF não encontrado');
+    //};
 
-    const myArray = ["entrada", "saida"];
-    if (!myArray.includes(bodyReq.tipo)) { // Validando tipo da operação
-      return res.status(400).send(`Tipo precisa ser "entrada" ou "saida"!`);
-    };
+    //const myArray = ["entrada", "saida"];
+    //if (!myArray.includes(bodyReq.tipo)) { // Validando tipo da operação
+//      return res.status(400).send(`Tipo precisa ser "entrada" ou "saida"!`);
+    //};
 
-    if (bodyReq.tipo === "saida" && bodyReq.value > 1000) { // Verificando se saida é maior que saldo
-      return res.status(400).send('Saldo insulficiente.');
-    };
+    //if (bodyReq.tipo === "saida" && bodyReq.value > 1000) { // Verificando se saida é maior que saldo
+//      return res.status(400).send('Saldo insulficiente.');
+//    };
 
     // Criar função para validar as duas casas decimais!!!
 
-    const operation = await Operation.create(bodyReq);
-    return res.send('status: Ok');
-  }
+//    const operation = await Operation.create(bodyReq);
+//    return res.send('status: Ok');
+//  }
 
   private async delete(req: Request, res: Response, next: NextFunction): Promise<Response> {
     const { id } = req.params;
