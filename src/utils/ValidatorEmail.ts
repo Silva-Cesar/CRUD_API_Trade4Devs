4805298295a704a2147292fd8d5f52e3ab9b3e5c
César Silva <cesaraugusto90@gmail.com>
2022-06-06 22:45:35 -0300

    Ajustar regex de validação de email

--- src/utils/ValidatorEmail.ts
+++ src/utils/ValidatorEmail.ts
@@ -1,9 +1,6 @@
 export class ValidatorEmail {
-
-    public static validator(email: string): boolean {
-            if ((!email.match(/^([a-z0-9_\.-]+\@[\da-z\.-]+\.[a-z\.]{2,6})$/gm))) { 
-                return false;
-            }
-            return true;
-        }
-    }
+  public static validator(email: string): boolean {
+      const emailRegex = /^([a-z0-9_\.-]+\@[\da-z\.-]+\.[a-z\.]{2,6})$/
+    return emailRegex.test(email);
+  }
+}
